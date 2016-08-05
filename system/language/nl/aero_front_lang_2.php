b02ad21f219042dfc2a649812e57e89f8a1d3613
mikepriest <mike.priest@10spd.com>
2016-08-04 19:54:23 -0700

    - Dutch language pack

---
+++ system/language/nl/aero_front_lang_2.php
@@ -0,0 +1,263 @@
+<?php
+//Non Admin
+$lang['ok']       = "OK";
+
+$lang['search']       = "Zoek...";
+$lang['searche']   = "Probeer een andere zoekterm.";
+$lang['allguides'] = "Alle Gidsen";
+$lang['onpage']       = "Op de pagina";
+$lang['found']    = "Resultaten gevonden.";
+
+//Tips
+$lang['finished']   = "Voltooi";
+$lang['gfinished']  = "Gids voltooid";
+$lang['next']       = "Volgende &raquo;";
+$lang['prev']       = "&laquo; Vorige";
+$lang['tdone']      = "Klaar";
+$lang['notfound']   = "Pagina niet gevonden";
+$lang['gfinish']    = "Gids voltooid!";
+$lang['congrats']   = "Gefeliciteerd je hebt de volgende gids voltooid: ";
+
+//Errors
+$lang['urlloop']    = "We kunnen de stap URL niet vinden. Om te voorkomen dat dit probleem zich herhaalt, eindigt de gids nu:";
+
+//Page not found
+$lang['oops']        = "Oeps. Je bent op de verkeerde pagina.";
+$lang['oopsdesc']    = "Het lijkt erop dat je op de verkeerde pagina bent beland. Maak een keuze hoe je verder wilt gaan. Je kunt terug naar de vorige stap of doorgaan naar de juiste pagina.";
+$lang['oopsm']      = "Ga terug";
+$lang['oopst']      = "Breng me naar de juiste pagina";
+
+//Share Guides
+$lang['share']      = "Deel";
+
+/**
+ *  Admin
+ */
+//Error
+$lang['requirede']    = "Controleer alle vereiste velden";
+
+//Menu System
+$lang['role']        = "Rol";
+$lang['roles']       = "Rollen";
+$lang['guide']       = "Gids";
+$lang['guides']          = "Gidsen";
+$lang['pathways']     = "Leerweg";
+
+//Buttons
+$lang['save']        = "Save";
+$lang['cancel']          = "Cancel";
+$lang['ok']             = "Ok";
+$lang['add']         = "Voeg toe";
+
+//Labels
+$lang['title']       = "Titel";
+$lang['desc']        = "Omschrijving";
+$lang['active']          = "Actief";
+
+$lang['stepdel']       = "Stap verwijderd F";
+$lang['stepdelconf']    = "Weet je zeker dat je deze stap wilt verwijderen? F";
+$lang['guidedelconf']   = "Weet je zeker dat je deze gids wilt verwijderen? F";
+
+/**
+ *  Guide admin nav
+ */
+//
+$lang['styling']           = "Stijling";
+$lang['navigation']        = "Navigatie";
+$lang['location']          = "Locatie";
+$lang['exceptions']        = "Uitzonderingen &amp; Validering";
+$lang['branching']         = "Branching";
+$lang['customcode']        = "Custom code";
+
+/**
+ *  Guide admin
+ */
+//Buttons
+$lang['guidea']       = "Creëer een gids";
+$lang['guideh']       = "Het lijkt erop dat je nog geen gidsen hebt toegevoegd. Klik \"Creëer een gids\" hierboven om een gids te starten.";
+
+//Basic
+$lang['basic']    = "Basis details";
+$lang['basicd']       = "Geef je gids een omschrijving zodat gebruikers begrijpen wat zij zullen leren in deze gids.";
+$lang['tags']     = "Tags";
+$lang['auto']     = "Auto Start (Voor Nieuwe Gebruikers)";
+$lang['autop']    = "Starten vanaf willekeurige pagina?";
+
+//Ratings
+$lang['rating']       = "Ratings";
+$lang['ratingd']   = "Lorem ipsum dolor sit.";
+$lang['est']      = "Geschatte tijd tot beëindiging (minuten)";
+$lang['diff']     = "Moeilijkheidsgraad";
+
+//Pathways
+$lang['pathd']    = "Voeg je gids aan een leerweg toe om course based learning te creëeren.";
+$lang['spath']    = "- Selecteer een leerweg -";
+
+//Roles
+$lang['roled']    = "Beperk de gids tot 1 of meerdere rollen.";
+
+/**
+ *  Steps admin
+ */
+$lang['richmedia']  = "Rich Media";
+$lang['quizzing']   = "Quizzing";
+$lang['youtube']    = "Youtube URL";
+$lang['embed']      = "Embed";
+
+//Tip Position
+$lang['bottom']     = "Onderaan (default)";
+$lang['top']        = "Bovenaan";
+$lang['left']       = "Links";
+$lang['right']      = "Rechts";
+$lang['orphan']     = "Orphan";
+
+//Tip sizes
+$lang['ssmall']     = "Klein (default)";
+$lang['smedium']    = "Medium";
+$lang['slarge']     = "Groot";
+$lang['sauto']      = "Auto";
+$lang['scustom']    = "Custom";
+
+//Colors
+$lang['trans']      = "Transparant (default)";
+$lang['red']        = "Rood";
+$lang['yellow']     = "Geel";
+$lang['green']      = "Groen";
+$lang['blue']       = "Blauw";
+$lang['black']      = "Zwart";
+
+//Step Navigation
+$lang['nnext']      = "Volgende is geklikt";
+$lang['nclick']     = "Item is geklikt";
+$lang['nrclick']    = "Item is geklikt met rechter muisknop";
+$lang['ndrag']      = "Item is gesleept";
+$lang['nhover']     = "Gezweefd over item";
+$lang['nkey']       = "Keyboard entry is beëindigd";
+$lang['npage']      = "Pagina is veranderd of vernieuwd";
+$lang['nvisible']   = "Volgende item is zichtbaar";
+
+$lang['step']      = "Stap";
+$lang['nstep']      = "Volgende stap (default)";
+
+//Hide buttons
+$lang['none']      = "Geen (default)";
+$lang['bnext']     = "Volgende knop";
+$lang['bprev']     = "Vorige knop";
+$lang['ball']      = "Alle knoppen";
+
+
+//URL validation
+$lang['uall']      = "Hele URL (default)";
+$lang['uanch']     = "Zonder Ankers (example.com/#something)";
+$lang['uparam']    = "Zonder Parameters (example.com?key=value)";
+$lang['uignor']    = "Negeer URL in zijn geheel";
+
+
+//Exceptions
+$lang['default']   = " (default)";
+$lang['xalert']      = "Alert";
+$lang['xskip']      = "Skip stap";
+$lang['xskipto']    = "Skip naar stap...";
+$lang['xback']      = "Ga terug naar de vorige stap";
+$lang['xmess']      = "Laat een boodschap zien";
+$lang['xno']        = "Doe niks";
+
+//Branching
+$lang['dontr']     = "Ga niet terug";
+
+//Buttons, titles, and descriptions
+$lang['done']     = "Ik ben klaar";
+$lang['stepa']        = "Voeg een stap toe";
+$lang['stepn']        = "Stap nummer";
+$lang['steph']     = "Het lijkt erop dat je nog geen stappen hebt toegevoegd. Voeg stappen toe door hierboven op \"Voeg een stap toe\" te klikken en vervolgens op een element op de pagina te klikken.";
+$lang['contente']   = "Bewerk Inhoud";
+$lang['contentd']   = "Omschrijf in detail wat de gebruiker ziet en hoe ze naar de volgende stap kunnen gaan; titels zijn zichtbaar in de stappenlijst in de sidebar.";
+$lang['content']       = "Inhoud";
+$lang['stylee']        = "Bewerk Stijl";
+$lang['styled']        = "Controleer wat de gebruiker ziet. Verander de tooltip grootte, posities, en dergelijke.";
+$lang['pos']       = "Positie";
+$lang['size']      = "Grootte";
+$lang['sizec']     = "Custom grootte (pixels)";
+$lang['stitle']        = "Show titel in tip?";
+$lang['sprev']     = "Show met vorige stap?";
+$lang['sbar']      = "Sluit sidebar?";
+$lang['ctip']      = "Contextuele tooltip?";
+$lang['nave']      = "Bewerk Navigatie";
+$lang['navd']      = "Of je nou de simpele “volgende” knop gebruikt of een meer geavanceerde gebruikers-interactie, jij beslist hoe de gebruikers van de ene naar de volgende stap gaan.";
+$lang['move']      = "Ga naar de volgende stap wanneer";
+$lang['moveto']        = "Ga naar de volgende stap";
+$lang['addm']     = "Voeg meer toe";
+$lang['buttonc']    = "Wil je de beschikbare knoppen in de tooltip veranderen?";
+$lang['hide']      = "Verbergen";
+$lang['loce']      = "Bewerk Locatie";
+$lang['uset']      = "Gebruik tekst?";
+$lang['locd']      = "Deze sectie is voor geavanceerde gebruikers. De hier beneden vertoonde informatie controleert hoe we de locatie van de tooltip or pagina bepalen.";
+$lang['loc']       = "Locator";
+$lang['find']      = "Stop Zoektocht";
+$lang['ra']           = "Meteen";
+$lang['dd']           = "Niet vertragen";
+$lang['ut']           = "Na";
+$lang['pause']     = "Zet zoektocht voort";
+$lang['wf']           = "Met";
+$lang['seconds']    = "seconde(n)";
+$lang['urls']      = "Manage hoe URLs jouw URLs zouden moeten valideren. Handig wanneer URLs complex of dynamisch zijn";
+$lang['url']       = "URL";
+$lang['urlv']      = "URL Validering";
+$lang['alerta']        = "Actie";
+$lang['excef']     = "Vind uitzondering";
+$lang['excel']     = "Verloren uitzondering";
+$lang['exceld']        = "Sommige elementen in de stappen kunnen veranderen door gebruikers interactie. Bijvoorbeeld, door weg te klikken van een dropdown menu of wanneer zij een popup window sluiten. Jij kunt beslissen hoe je met dit soort gebeurtenissen om wilt gaan.";
+$lang['alertm']        = "Wat wil je dat jouw boodschap zegt?";
+$lang['skiptom']    = "Naar welke stap wil je skippen?";
+$lang['branch']        = "Branch naar een andere gids";
+$lang['branchd']    = "Wanneer er beslissingspunten zijn in jouw processen, maak gebruik van branching om je gidsen simpel en makkelijk te behapstukken te houden.";
+$lang['bt']           = "Branch naar gids";
+$lang['br']       = "Ga terug naar stap";
+$lang['bre']          = "Wil je de gebruiker terug dirigeren naar deez gids wanneer hij/zij de branch heeft voltooid?";
+$lang['bs']           = "Voordat de stap zichtbaar is";
+$lang['os']           = "Zodra de stap zichtbaar is";
+$lang['bh']           = "Voordat de stap verborgen is";
+$lang['oh']           = "Zodra de stap verborgen is";
+$lang['exceptions'] = "Uitzonderingen";
+$lang['validation'] = "Validering";
+$lang['exce']      = "Uitzonderingen &amp; Validering";
+$lang['val1']      = "a - Vertegenwoordigt een alfanumeriek teken (A-Z,a-z)";
+$lang['val2']      = "9 - Vertegenwoordigt een alfanumeriek nummer (0-9)";
+$lang['val3']      = "* - Vertegenwoordigt alle alfanumeriek karakters (A-Z,a-z,0-9)";
+$lang['val4']      = "Bekijk gedetailleerde documentatie";
+$lang['excd']      = "Stappen zijn mogelijk niet zichtbaar voor verscheidene redenen (incorrecte toestemming, setup benodigd, etc.). Jij kunt beslissen wat te doen wanneer er een stap mist.";
+$lang['vald']      = "Pas jouw velden aan met behulpzame validering en input maskering.";
+$lang['mask']      = "Input Maskering (e.g. (999) 999-9999)";
+$lang['codee']     = "Custom code";
+$lang['coded']     = "Gebruik JavaScript om de benodigde aanpassingen aan jouw stappen te maken - deze sectie is alleen voor de geavanceerde gebruiker.";
+$lang['before']     = "Voordat";
+$lang['after']      = "Nadat";
+
+//Quiz
+$lang['quizd']  = "Om een quiz te creëeren; gebruik de tip inhoud om jouw vraag toe te voegen. Vervolgens voeg je de antwoorden toe and selecteer je het correcte antwoord hier.";
+$lang['quizad']  = "Type een antwoord en press enter of klik op de toevoeg knop. Om het juiste antwoord the selecteren, check de checkbox naast de delete knop.";
+$lang['quizcontent']  = "Quiz Inhoud";
+$lang['question']  = "Vraag";
+$lang['answers']  = "Antwoorden";
+$lang['answera']  = "Voeg een antwoord toe";
+$lang['chooselay']  = "Kies Layout";
+$lang['qerrmsg']  = "Custom Error Boodschap";
+$lang['qerrmsgd']  = "Voeg een custom boodschap toe om de gebruiker uit te leggen waarom zijn/haar antwoord fout is.";
+
+$lang['start']   = "Een Gids starten";
+$lang['startd']   = "Gidsen kunnen ten alle tijde gestart worden door gebruik te maken van de gidsen sidebar. Soms is het echter van belang om gidsen op een andere manier te starten.";
+$lang['restrictd']   = "Beperk element gebruik voor niet getrainde gebruikers";
+$lang['restrictc']   = "Overlap kleur";
+$lang['infod']       = "Animeer tooltip";
+
+$lang['restrictt']   = "Functie beperkt";
+$lang['restrictb']   = "Voor deze functie heb je begeleiding nodig wanneer je deze voor de eerste keer gebruikt.";
+
+//Quiz layout
+$lang['onec']       = "1 column (default)";
+$lang['twoc']       = "2 column";
+$lang['threec']     = "3 column";
+$lang['fourc']      = "4 column";
+
+/* End of file injected_lang.php */
+/* Location: ./system/language/english/injected_lang.php */
---
+++ system/language/nl/aero_lang_2.php
@@ -0,0 +1,279 @@
+<?php
+
+//Copyright
+$lang['copy']         = "TenSpeed Technologies, Inc. &copy; 2015 All rights reserved.<br>You are running version";
+$lang['tagline']        = "Revolutionizing how <strong>you</strong> onboard, support and educate your users";
+
+//Login
+$lang['luser']         = "Gebruikersnaam/Email";
+$lang['lpass']         = "Wachtwoord";
+$lang['signin']         = "Inloggen";
+
+$lang['signin']         = "Inloggen";
+$lang['reset']          = "Reset";
+$lang['passreset']      = "Wachtwoord vergeten? Klik hier om je wachtwoord te resetten";
+$lang['badpass']        = "Ongeldige gebruikersnaam of wachtwoord";
+
+$lang['notfound']       = "Pagina niet gevonden";
+$lang['notfounddesc']   = "Deze pagina bestaat niet of je hebt geen toegang tot deze pagina. Neem contact op met jouw administrator wanneer je denkt dat dit niet klopt.";
+
+$lang['installgood']    = "Jouw Installatie was Succesvoll!";
+$lang['installdesc']    = "Om te beginnen heb je eerst een administrator JumpSeat account nodig: Enter een e-mailadres en wachtwoord en wij creëeren jouw eerste gebruiker:";
+
+//Menu System
+$lang['home']        = "Home";
+$lang['analytics']    = "Analytics";
+$lang['role']        = "Rol";
+$lang['roles']       = "Rollen";
+$lang['guide']       = "Gids";
+$lang['guides']          = "Gidsen";
+$lang['pathway']      = "Leerweg";
+$lang['spathway']     = "Selecteer een leerweg";
+$lang['pathways']     = "Leerwegen";
+$lang['config']          = "Configuratie";
+$lang['blacklist']    = "URL Blacklisting";
+$lang['version']      = "Versie";
+$lang['versions']     = "Versies";
+$lang['logout']          = "Logout";
+
+$lang['pagedata']     = $lang['config'];
+$lang['pathwaymap']       = $lang['pathways'];
+$lang['rolemap']      = $lang['roles'];
+
+//Buttons
+$lang['save']        = "Save";
+$lang['saveclose']    = "Save &amp; Sluit";
+$lang['cancel']          = "Cancel";
+$lang['ok']             = "Ok";
+$lang['add']         = "Voeg toe";
+$lang['delete']          = "Delete";
+$lang['edit']        = "Bewerk";
+$lang['clone']       = "Clone";
+$lang['export']          = "Export";
+$lang['import']          = "Import";
+
+//Labels
+$lang['title']       = "Titel";
+$lang['desc']        = "Omschrijving";
+$lang['host']        = "Host";
+$lang['permissions']   = "Toestemmingen";
+$lang['multiactions']  = "Met geselecteerde...";
+$lang['sguide']          = "Selecteer een gids";
+$lang['srole']       = "Selecteer een rol";
+$lang['yes']         = "Ja";
+$lang['no']             = "Nee";
+
+//Error
+$lang['requirede']    = "Check alle vereiste velden";
+$lang['success']      = "Succes! Jouw wijzigingen zijn opgeslagen.";
+
+//Tools
+$lang['selected']       = "Gids(en) Geselecteerd";
+$lang['case']       = "Casus gevoelig?";
+
+//Common Field Labels
+$lang['description']  = "Omschrijving";
+$lang['modified']     = "Gemodificeerd";
+$lang['firstname']    = "Voornaam";
+$lang['lastname']     = "Achternaam";
+$lang['created']      = "Gecreëerd";
+$lang['lastlogin']    = "Laatst ingelogd";
+$lang['sysadmin']     = "System Admin";
+$lang['creator']      = "Creator";
+$lang['name']         = "Naam";
+$lang['tools']        = "Tools";
+$lang['replace']       = "Vervangen";
+$lang['asdasd']       = "asdasd";
+
+/**
+ *  Apps
+ */
+
+$lang['hometitle']    = "JUMPSEAT";
+$lang['apps']        = "Applicaties";
+$lang['appsc']       = "Huidige Applicaties";
+$lang['appsd']       = "Onderaan staan alle applicaties in jouw organisatie die gebruik maken van JumpSeat opgesomd. Om een nieuwe applicatie toe te voegen, klik \"VOEG EEN NIEUWE WEB APPLICATIE TOE\" hieronder.";
+$lang['active']          = "Actief";
+$lang['status']          = "Applicatie status";
+$lang['etitle']          = "Enter een titel";
+$lang['edesc']       = "Enter een omschrijving";
+$lang['vguides']      = "Bekijk Gidsen";
+$lang['vpaths']          = "Bekijk leerwegen";
+$lang['on']             = "Aan";
+$lang['off']         = "Uit";
+$lang['appsa']       = "Voeg een nieuwe web applicatie toe";
+$lang['addhost']      = "Voeg een andere host toe";
+$lang['more']        = "meer";
+
+
+/**
+ *  Blacklist
+ */
+$lang['prefix']          = "Prefix";
+$lang['suffix']          = "Suffix";
+$lang['eurl']        = "Enter een URL";
+$lang['matchpre']     = "Match met een prefix";
+$lang['matchsuf']     = "Match met een suffix";
+
+/**
+ *  Guides
+ */
+$lang['guidesd']      = "Als je een guide wilt creëeren zonder enige stappen-heisa, dat kan hier.  Creëer een lege gids en klik vervolgens de \"stappen\" knop om de guide te starten.";;
+$lang['versionsd']    = "De laatste versie is momenteel in gebruik. “Herstellen” zal de oudere gids kopiëren en transformeren naar de huidige versie. U kunt ongewilde versies verwijderen of aanpassen.";
+$lang['trashd']         = "Herstel gidsen uit de prullenbak en delete ongewilde gidsen.";
+$lang['addg']        = "Creëer een nieuwe gids";
+$lang['trash']       = "Prullenbank";
+$lang['importg']      = "Import gidsen";
+$lang['etitle']          = "Enter een titel";
+$lang['edesc']       = "Enter een omschrijving";
+$lang['afindr']          = "Vind &amp; Vervang";
+$lang['afindp']         = "Gids eigenschap";
+$lang['afindc']         = "Vind";
+$lang['afindrw']        = "Vervangen Met";
+$lang['afindp']         = "Voorbeeldweergave";
+
+$lang['sprop']          = "- Selecteer een eigenschap -";
+$lang['sgtitle']        = "Gids: Titel";
+$lang['sgdesc']         = "Gids: Omschrijving";
+$lang['sstitle']        = "Stap: Titel";
+$lang['ssbody']         = "Stap: Inhoud";
+
+
+
+/**
+ *  Roles
+ */
+$lang['assocp']          = "Voeg een nieuwe leerweg toe";
+$lang['addr']        = "Creëer een nieuwe rol";
+$lang['roled']       = "Op deze pagina kun je de gidsen, leerwegen, en het succes van de gebruikers managen. Zodra je een rol hebt gecreëerd (door op creëeren te klikken), klik op de gebruikers icoon om de toegangspagina van de rol te openen.";
+$lang['cardr']       = "Manage rol";
+
+/**
+ *  Pathways
+ */
+$lang['addp']        = "Creëer een nieuwe leerweg";
+$lang['pathwaysd']    = "Leerwegen zijn zo gestructureerd dat de gebruiker door een proces loopt van begin tot voltooiing. Voeg een nieuwe leerweg toe en sleep het heen en weer om zo de leerweg te herstructureren.";
+$lang['pathd']       = "Leerwegen helpen gebruikers om de belangrijkste taken en gerelateerde concepten in a chronologische volgorde te leren. Een leerweg kan een variable nummer aan gidsen bevatten. Om een nieuwe leerweg toe te voegen, klik the “voeg toe” knop hieronder.";
+$lang['cardp']       = "Manage leerweg";
+$lang['assocg']          = "Voeg een gids toe";
+$lang['assocr']          = "Voeg een rol toe";
+$lang['start']       = "Start";
+$lang['end']         = "Eind";
+
+
+/**
+ * Mappings
+ */
+$lang['pathmap']      = "Leerwegen geassocieerd met deze rol";
+$lang['pathmapd']     = "Controleer wie toegang heeft tot jouw leerwegen. Voeg een leerweg toe aan een rol(len) zodat alleen de gebruikers in die groep(en) toegang hebben. De leerweg is toegankelijk voor alle gebruikers wanneer een leerweg geen onderdeel is van een rol!";
+$lang['pathguide']        = "Gidsen geassocieerd met deze rol";
+$lang['pathguided']    = "Voeg een gids toe aan deze rol en gebruikers in die groep hebben toegang. De gids is toegangelijk voor alle gebruikers wanneer de gids geen onderdeel is van een rol!";
+$lang['rolethings']    = "Dingen die deze rol kan...";
+$lang['rolethingsd']   = "Toegang tot administratieve functies kunnen makkelijk beperkt worden. Klik op de opties hier beneden en alles wordt automatisch geupdate.";
+$lang['usermap']      = "Gebruikers geassocieerd met deze rol";
+$lang['usermapd']     = "Kies welke gebruikers tot deze rol behoren. Voeg gebruikers toe door op de \"Voeg een gebruiker toe\" knop te klikken en zij zullen toegang hebben tot alles dat tot deze rol behoort.";
+
+/**
+ *  Page data
+ */
+$lang['importc']      = "Import URLs";
+$lang['addd']        = "Voeg een dynamische url toe";
+$lang['configd']      = "Gidsen dwingen automatisch een-op-een URL matching af. Dynamische URL's bevatten dingen zoals gebruikersnaam, bedrijfsnaam (multi-tenant), e-mailadres of een unieke identifier. Je kunt 1 gids creëeren voor alle dynamische URLs door gebruik te maken van onze dynamische URL vervanging.";
+$lang['basicconfigd']  = "Deze sectie is alleen voor geavanceerde gebruikers.";
+$lang['required']     = "Vereist";
+$lang['requiredhelp']  = "JavaScript e.g. MYAPP";
+$lang['fire']  = "Fire";
+$lang['firehelp']  = "Enter JavaScript to fire some code voordat JumpSeat begint";
+$lang['username']     = "Gebruikersnaam";
+$lang['usernamehelp']  = "JavaScript e.g. MYAPP.constants.USERNAME";
+$lang['roleshelp']    = "JavaScript e.g. MYAPP.constants.ROLES";
+$lang['warnadvanced']  = "Waarschuwing! Dit is alleen voor geavanceerde gebruikers!";
+$lang['basicconf']    = "Basic Configuratie";
+$lang['urlsubstitution']= "Dynamische URLs";
+$lang['regex']           = "Vervang (regex)";
+$lang['eregex']       = "Enter een regex";
+$lang['with']        = "Met";
+$lang['ewith']           = "JavaScript e.g. MYAPP.dynamicValue";
+
+/**
+ *  Permissions
+ */
+$lang['steps']           = "Stappen";
+
+/**
+ *  Blacklist
+ */
+$lang['url']          = "URL";
+$lang['addurl']       = "Voeg een URL toe";
+$lang['blacklistd']    = "De gidsen-bar is zichtbaar op alle URLs onder dit domein. Als je pagina’s wilt negeren (e.g. login page), zul je die pagina’s hier moeten blacklisten.";
+
+/**
+ *  Users
+ */
+$lang['admin']       = "System Administrator";
+$lang['user']        = "Gebruiker";
+$lang['users']           = "Gebruikers";
+$lang['usersd']       = "Manage wie toegang heeft tot deze administration console. System Administrators kunnen de gebruikers, applicaties en hun data managen.";
+$lang['addu']        = "Voeg een gebruiker toe";
+$lang['importu']      = "Import Gebruikers";
+
+$lang['firstname']        = "Voornaam";
+$lang['firstnamep']    = "Enter een voornaam";
+$lang['lastname']     = "Achternaam";
+$lang['lastnamep']        = "Enter een achternaam";
+$lang['email']           = "E-mail";
+$lang['emailaddress']  = "E-mailadres";
+$lang['emailp']       = "Enter e-mailadres";
+$lang['passwordc']        = "Veranderd wachtwoord";
+$lang['password']     = "Wachtwoord";
+$lang['passwordv']        = "Bevestig nieuw wachtwoord";
+$lang['passwordp']        = "Enter een wachtwoord";
+$lang['passwordvp']    = "Bevestig wachtwoord";
+$lang['profilec']     = "Bewerk profiel";
+$lang['emailuser'] = "E-mail gebruiker";
+
+/**
+ *  Analytics
+ */
+$lang['reportusers']   = "Begonnen vs Voltooid";
+$lang['reportusersd']  = "Verschil tussen gebruikers die zijn begonnen en die gidsen hebben voltooid";
+
+$lang['reportprogress']    = "Gebruikers voortgang";
+$lang['reportprogressd']   = "Een grafiek om de voortgang van gebruikers te laten zien in alle gidsen";
+
+$lang['filteruser']        = "Rapporteer bij gebruiker";
+$lang['nouser']            = "Geen gebruiker geselecteerd";
+
+/**
+ *  Datatables
+ */
+$lang['dtfilter']          = "Zoek F";
+$lang['dtfilterplace']         = "any column F";
+$lang['dtresults']             = "Showing F";
+$lang['dtshowing']             = "Showing F";
+$lang['dtnext']            = "Volgende F";
+$lang['dtprevious']        = "Vorige F";
+
+//  *******  Do not change _MENU_  ********
+$lang['dtentries']             = "Show _MENU_ entries F";
+$lang['dtfiltered']        = " (filtered from _MAX_ total entries) F";
+$lang['dtinfo']            = "Showing _START_ to _END_ of _TOTAL_ entries F";
+$lang['dtempty']            = "No data available in table F";
+$lang['dtinfoempty']        = "Showing 0 to 0 of 0 entries F";
+$lang['dtzero']             = "No matching records found F";
+
+/**
+ *  Features
+ */
+$lang['features']  = "Functies";
+$lang['featuresd'] = "Functies maken het voor ons mogelijk om non-browser applicatie schermen te kopiëren voor training doeleinden. Een functie is een collectie van screenshots 
+Features allow us to replicate non-browser application screens for the purpose of training. A feature is a collection of screen-shots that makeup an emulated process. Create screen-shots for each step within your process";
+$lang['addf']  = "Create a new feature";
+
+$lang['pages'] = "Pages";
+$lang['pagesd']    = "Pages belong to a feature and should contain a screen-shot for that step of the process.";
+$lang['addpage'] = "Create a new page";
+
+
+/* End of file aero_lang.php */
+/* Location: ./system/language/english/aero_lang.php */

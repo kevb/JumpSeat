3a4a22a36dd7b8912606876b00fb6c14db063d22
mike.priest <mike.priest@triforklearning.com>
2018-01-17 16:36:51 -0700

    Pathway bug fix for guide count

--- application/controllers/api/users.php
+++ application/controllers/api/users.php
@@ -104,12 +104,11 @@ class Users extends REST_Controller
      */
     function install_post()
     {
-        $this->user_model->create_admin();
-
         if($this->user_model->count() > 1){
             $this->response(array('error' => 'Function not allowed'), 404);
             exit;
         }
+        $this->user_model->create_admin();
 
         $user = array(
             "email" => $this->post('email'),
